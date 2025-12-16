2eabd0969f2054b0ba25645d48b0730f0400bd47
Ömer Faruk Çetin <ofarukcetin@gmail.com>
2025-12-16 15:10:02 +0300

    fix: Remove AsyncStorage import from firebaseConfig for web compatibility

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from "firebase/firestore";
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
